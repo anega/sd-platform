3980f72872e3f1c1422a2db22101f28a2867f24c
anegin <anegamega@gmail.com>
2015-11-25 22:17:09 +0200

    Add jquery data-table

--- js/script.js
+++ js/script.js
@@ -1,31 +1,38 @@
-// Top banner
-$(window).load(function() {
-    $('#banner').flexslider({
-        animation: "slide",
-        controlNav: false,
-        directionNav: false
+(function (window, $) {
+// Homepage top banner-slider
+    $(window).load(function() {
+        $('#banner').flexslider({
+            animation: "slide",
+            controlNav: false,
+            directionNav: false
+        });
     });
-});
 
 // Photo section slider
-$(window).load(function() {
-    // The slider being synced must be initialized first
-    $('#carousel').flexslider({
-        animation: "slide",
-        controlNav: false,
-        directionNav: false,
-        animationLoop: true,
-        slideshow: false,
-        itemWidth: 174,
-        itemMargin: 0,
-        asNavFor: '#slider'
+    $(window).load(function() {
+        // The slider being synced must be initialized first
+        $('#carousel').flexslider({
+            animation: "slide",
+            controlNav: false,
+            directionNav: false,
+            animationLoop: true,
+            slideshow: false,
+            itemWidth: 174,
+            itemMargin: 0,
+            asNavFor: '#slider'
+        });
+
+        $('#slider').flexslider({
+            animation: "slide",
+            controlNav: false,
+            animationLoop: false,
+            slideshow: false,
+            sync: "#carousel"
+        });
     });
 
-    $('#slider').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        sync: "#carousel"
+// jQuery data-tables plugin
+    $(document).ready(function(){
+        $('#sd-uploads').DataTable();
     });
-});
+}(window, jQuery));
